1902070eb5329ef090475554e4a184a0f972b13d
YILMAZ\Emre YILMAZ <cemalemreyilmaz@gmail.com>
2017-05-28 15:52:42 +0300

    only for training

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FacialFeatures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnSelectPhoto;
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tbPersonName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbSelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            btnSelectPhoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectPhoto
+            // 
+            btnSelectPhoto.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            btnSelectPhoto.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btnSelectPhoto.Location = new System.Drawing.Point(180, 294);
+            btnSelectPhoto.Name = "btnSelectPhoto";
+            btnSelectPhoto.Size = new System.Drawing.Size(125, 23);
+            btnSelectPhoto.TabIndex = 0;
+            btnSelectPhoto.Text = "Select  Photo";
+            btnSelectPhoto.UseVisualStyleBackColor = false;
+            btnSelectPhoto.Click += new System.EventHandler(this.btnOpenPhoto_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 257);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG (*.jpg)|*.jpg|Windows bitmap (*.bmp)|*.bmp|All files|*.*";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(353, 276);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(116, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(13, 276);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(115, 95);
+            this.listBox2.TabIndex = 3;
+            // 
+            // tbPersonName
+            // 
+            this.tbPersonName.Location = new System.Drawing.Point(484, 213);
+            this.tbPersonName.Multiline = true;
+            this.tbPersonName.Name = "tbPersonName";
+            this.tbPersonName.Size = new System.Drawing.Size(121, 20);
+            this.tbPersonName.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(180, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(125, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbSelect
+            // 
+            this.cbSelect.FormattingEnabled = true;
+            this.cbSelect.Items.AddRange(new object[] {
+            "Igrenme",
+            "Kizgin",
+            "Korku",
+            "Mutlu",
+            "Notr",
+            "Saskin",
+            "Uzgun"});
+            this.cbSelect.Location = new System.Drawing.Point(484, 249);
+            this.cbSelect.Name = "cbSelect";
+            this.cbSelect.Size = new System.Drawing.Size(121, 21);
+            this.cbSelect.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(493, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Person\'s Name";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(484, 13);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(125, 49);
+            this.btnTest.TabIndex = 8;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 442);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSelect);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbPersonName);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(btnSelectPhoto);
+            this.Name = "Form1";
+            this.Text = "Facial Features";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox tbPersonName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Luxand;
+using System.IO;
+
+namespace FacialFeatures
+{
+    public partial class Form1 : Form
+    {
+        // WinAPI procedure to release HBITMAP handles returned by FSDKCam.GrabFrame
+        [DllImport("gdi32.dll")]
+        static extern bool DeleteObject(IntPtr hObject);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double[] dizi2 = new double[132];
+        List<string> list = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (FSDK.FSDKE_OK != FSDK.ActivateLibrary("bq39OVODkxU5WnoCPMjrCrklbBDY7dFbEZF+KwLbS7nVsp7DMRQbpXYM/+DiANnEOu8FdWaTe2sSUhknwohMI5WOpKSUrXu1v/COtp30YSUdisb6SUKUIg6lI8SvNeG071dS75EoeTbLGx13QOSdG/pifUqTU1u59iSGcy4oP3Y="))
+            {
+                MessageBox.Show("Please run the License Key Wizard (Start - Luxand - FaceSDK - License Key Wizard)", "Error activating FaceSDK", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            FSDK.InitializeLibrary();
+            FSDK.SetFaceDetectionParameters(true, true, 384);
+        }
+
+        private void btnOpenPhoto_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FSDK.CImage image = new FSDK.CImage(openFileDialog1.FileName);
+
+                    // pencere genişliğine uyacak şekilde yeniden boyutlandırma
+                    double ratio = System.Math.Min((pictureBox1.Width + 0.4) / image.Width,
+                        (pictureBox1.Height + 0.4) / image.Height);
+                    image = image.Resize(ratio);
+
+                    Image frameImage = image.ToCLRImage();
+                    Graphics gr = Graphics.FromImage(frameImage);
+
+                    FSDK.TFacePosition facePosition = image.DetectFace();
+                    if (0 == facePosition.w)
+                        MessageBox.Show("No faces detected", "Face Detection");
+                    else
+                    {
+                        int left = facePosition.xc - (int)(facePosition.w * 0.6f);
+                        int top = facePosition.yc - (int)(facePosition.w * 0.5f);
+                        gr.DrawRectangle(Pens.LightGreen, left, top, (int)(facePosition.w * 1.2), (int)(facePosition.w * 1.2));
+
+                        FSDK.TPoint[] facialFeatures = image.DetectFacialFeaturesInRegion(ref facePosition);
+                        int i = 0, j = 0;
+                        for (int k = 0; k < 132; k = k + 2)
+                        {
+                            dizi2[k] = facialFeatures[j].x;
+                            j++;
+                        }
+                        j = 0;
+                        for (int t = 1; t < 132; t = t + 2)
+                        {
+                            dizi2[t] = facialFeatures[j].y;
+                            j++;
+                        }
+                        foreach (FSDK.TPoint point in facialFeatures)
+                        {
+                            gr.DrawEllipse((++i > 2) ? Pens.LightGreen : Pens.Blue, point.x, point.y, 3, 3);
+                            pictureBox1.Image = frameImage;
+                            listBox1.Items.Add(point.x);
+                            listBox2.Items.Add(point.y);
+                            list.Add(point.x.ToString() + "," + point.y.ToString());
+                        }
+
+                        gr.Flush();
+                        // display image
+
+                        pictureBox1.Refresh();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception");
+                }
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = tbPersonName.Text;
+                string klasor = cbSelect.Text.ToString();
+                string path = @"C:\Users\Emre YILMAZ\Documents\Mimik Algılama\" + klasor + "\\" + name + ".txt";
+
+                if (File.Exists(path))
+                {
+                    MessageBox.Show("Kaydedilmek istenen dosya bulunmakta");
+                }
+                else
+                {
+                    
+                    using (System.IO.StreamWriter file =
+    new System.IO.StreamWriter(path))
+                    {
+                        foreach (string line in list)
+                        {
+
+                            file.WriteLine(line);
+
+                        }
+                        MessageBox.Show("Resim Başarıyla Eklendi");
+                        list.Clear();
+                        tbPersonName.Clear();
+                        listBox1.Items.Clear();
+                        listBox2.Items.Clear();
+                        pictureBox1.Image = null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception");
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
